690af1ac92971a695c9824d05ca4bffa86e44b4c
mo5 <163494348+mont500@users.noreply.github.com>
2024-03-21 21:40:35 -0500

    Added jump script

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody rb;
-    public float moveSpeed;
+    public float moveSpeed = 1000f;
+    public float jumpSpeed = 2000f;
 
     bool forwardKey = false;
     bool backKey = false;
     bool rightKey = false;
     bool leftKey = false;
+    bool spaceKey = false;
 
     // Start is called before the first frame update
     void Start()
@@ -56,6 +58,15 @@ public class PlayerController : MonoBehaviour
         {
             leftKey = false;
         }
+
+        if (Input.GetKey("space"))
+        {
+            spaceKey = true;
+        }
+        else
+        {
+            spaceKey = false;
+        }
     }
 
     void FixedUpdate()
@@ -78,6 +89,12 @@ public class PlayerController : MonoBehaviour
         if (leftKey == true)
         {
             rb.AddForce(-moveSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (spaceKey == true)
+        {
+            Debug.Log("space is pressed");
+            rb.AddForce(Vector3.up * jumpSpeed * Time.deltaTime);
         }
     }
 }
